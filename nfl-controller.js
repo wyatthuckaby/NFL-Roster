d74c62d62328da1ee16ed3c4701b58a21ca45b0f
Wyatt Huckaby <huckabywyatt@gmail.com>
2017-08-07 23:36:57 -0600

    Added form in controller and connected render

--- nfl-controller.js
+++ nfl-controller.js
@@ -7,9 +7,38 @@ function NflController() {
 	var service = new NflService();
 
 	
-	function renderPlayers(){
-		//this is where we can render the players and make shit happen
+	function renderPlayers(foundPlayers){
+
+		var selectableTemplate = ``;
+
+
+
+		//it would be a horrible idea to just plain print these out onto the render context.
+		//just the computing power it would take to push that is reason enough not too.
+		//so instead, we will only get the filtered players. 
+		foundPlayers.forEach(player =>{
+			console.log (player.firstname, player.lastname, player.pro_team)
+		});
+		document.getElementById("selectable-players").innerHTML = selectableTemplate;
 
 	}
+
+	/**
+	 * search players function
+	 * @param  {"event"} e event
+	 * 
+	 */
+	this.searchPlayers = function(e){
+		e.preventDefault(); //stops default reload
+
+		service.searchPlayers(
+			e.target["name-search"].value,
+			e.target["team-search"].value,
+			e.target["pos-search"].value,
+			renderPlayers);
+		//console.log (e.target.name-search.value);
+
+	}
+
 }
 
--- nfl-service.js
+++ nfl-service.js
@@ -6,6 +6,10 @@ function NflService() {
      */
     var players = [];
 
+    /**
+     * Loads player data either from localStorage or nfl API.
+     * @return {void} not applicable
+     */
     function loadPlayerData(){
         var localst = localStorage.getItem("BCWLocalNFL");
         if (localst){
@@ -27,11 +31,83 @@ function NflService() {
     }
 
 
+
+    //NOTE: If running on ES5 syntax be sure to replace the filter
+    //callbacks below with actuall functions. '=>' is only es6 compliant.
     /**
-     * @return {value based player object}
+     * Filters through the player objects by team
+     * @param  {string} team query
+     * @return {array}  results
+     */
+    function getPlayersByTeam(team){
+        var ret = [];
+        players.filter(player => {
+            if (player.pro_team === team) 
+                ret.push(player);
+            
+        });
+        return ret;
+    }
+
+
+    /**
+     * Filters through the player objects by field position
+     * @param  {string} pos query
+     * @return {array}   results
+     */
+    function getPlayersByPosition  (pos){
+        var ret = [];
+        players.filter(player => {
+            if (player.position === pos) 
+                ret.push(player)
+        });
+
+
+        return ret;
+    }
+
+
+    /**
+     * Filters players by name
+     * @param  {string} name query
+     * @return {array}      results
+     */
+    function getPlayersByName(name){
+        var ret =[];
+        //i want to parse both the first and last name at the same time
+        //for the maximum chance for the user finding the result they want.
+        players.filter(player => {
+            if (player.firstname === name || player.lastname === name)
+                ret.push(player);
+        });
+
+        return ret;
+    }
+
+
+    /**
+     * Returns value seperated player object.
+     * @return {array} players 
      */
     this.getPlayers = function() {
         return JSON.parse(JSON.stringify(players)); 
+    }
+
+    this.searchPlayers = function (name,team,pos,cb){
+        var foundPlayers = [];
+        //check if anything isnt quite right.
+        if (!name && !team && !pos) return; /*TODO Find better way to do this*/
+        if (name){
+            foundPlayers = foundPlayers.concat(getPlayersByName(name));
+        }
+        if (team) {
+            foundPlayers = foundPlayers.concat(getPlayersByTeam(team));
+        }
+        if (pos) {
+            foundPlayers = foundPlayers.concat(getPlayersByPosition(pos));
+        }
+
+        cb(foundPlayers);
     }
 
 
